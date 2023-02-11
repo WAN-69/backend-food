09e6cf6899a0f783c30dcb7ab80f15e52a4d5438
unknown <irwan.nepang@gmail.com>
2023-02-11 22:09:31 +0700

    first init

---
+++ app/Http/Controllers/FoodController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Food;
+use Illuminate\Http\Request;
+
+class FoodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Food::all();
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Food::updateOrCreate([
+            'name' => $request->name
+        ],[
+            'price' => $request->price,
+            'url_image' => $request->url_image->store('food'),
+            'deskripsi' => $request->deskripsi
+        ]);
+
+        return response()->json([
+            'status' => 'Ok',
+            'messages' => 'Data Berhasil Disimpan'
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Food  $food
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Food $food)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Food  $food
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Food $food)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Food  $food
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Food $food)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Food  $food
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Food $food)
+    {
+        $food->delete();
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Berhasil dihapus'
+        ], 200);
+    }
+}
